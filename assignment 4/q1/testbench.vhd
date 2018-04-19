a338d5b4ef096798a483b42fb0165b3265772a25
SayantanRC <bin.sayantan@gmail.com>
2018-04-19 13:37:11 +0530

    Create testbench.vhd

---
+++ assignment 4/q1/testbench.vhd
@@ -0,0 +1,50 @@
+-- Testbench
+library IEEE;
+use IEEE.std_logic_1164.all;
+ 
+entity testbench is
+-- empty
+end testbench; 
+
+architecture tb of testbench is
+
+-- DUT component i.e. Device Under Test
+component or_gate is
+port(
+  A: in BIT;
+  B: in BIT;
+  X: out BIT);
+end component;
+
+signal a1, b1, x1: BIT;
+
+begin
+
+  -- Connect DUT
+  DUT: or_gate port map(a1, b1, x1);
+
+  process
+  begin
+    a1 <= '0';
+    b1 <= '0';
+    wait for 30 ns;
+  
+    a1 <= '0';
+    b1 <= '1';
+    wait for 30 ns;
+
+    a1 <= '1';
+    b1 <= '0';
+    wait for 30 ns;
+
+    a1 <= '1';
+    b1 <= '1';
+    wait for 30 ns;
+    
+    -- Clear inputs
+    a1 <= '0';
+    b1 <= '0';
+    
+    wait;
+  end process;
+end tb;
